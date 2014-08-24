0ccc83c77e0f1aec7dd418349bb708401da2db26
testkitschen <katryn.mcintosh@gmail.com>
2014-08-24 01:24:14 -0400

    Can show and hide mouth and eye drawers

--- app/assets/javascripts/parts-buttons.js
+++ app/assets/javascripts/parts-buttons.js
@@ -1,8 +1,17 @@
-// $( "#eyecategory" ).click(function() {
-//   event.preventDefault();
-//   if ( $( "#eyes:first" ).is( ":hidden" ) ) {
-//     $( "eyes" ).show( "slow" );
-//   } else {
-//     $( "eyes" ).slideUp();
-//   }
-// });
+$( "#eyecategory" ).click(function() {
+
+  if ( $( "#eyes:first" ).is( ":hidden" ) ) {
+    $( "#eyes" ).show( "slow" );
+  } else {
+    $( "#eyes" ).slideUp();
+  }
+});
+
+$( "#mouthcategory" ).click(function() {
+
+  if ( $( "#mouths:first" ).is( ":hidden" ) ) {
+    $( "#mouths" ).show( "slow" );
+  } else {
+    $( "#mouths" ).slideUp();
+  }
+});
