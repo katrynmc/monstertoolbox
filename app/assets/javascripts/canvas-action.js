e87e715cf0c63ea49f1d8af35442bf5667ccefb9
testkitschen <katryn.mcintosh@gmail.com>
2014-08-24 00:21:13 -0400

    Back in biz...You can add images to the canvas yay

--- app/assets/javascripts/canvas-action.js
+++ app/assets/javascripts/canvas-action.js
@@ -1,50 +1,29 @@
-var canvas, stage;
+var canvas;
 var mouseTarget;  // the display object currently under the mouse, or being dragged
 var dragStarted;  // indicates whether we are currently in a drag operation
 var offset;
 var update = true;
 var index = 0;
 
-function init() {
-  // create stage and point it to the canvas:
-  canvas = document.getElementById("monsterCanvas");
-  stage = new createjs.Stage(canvas);
-
-  // enable touch interactions if supported on the current device:
-  createjs.Touch.enable(stage);
-
-  // enabled mouse over / out events
-  stage.enableMouseOver(10);
-  stage.mouseMoveOutside = true; // keep tracking the mouse even when it leaves the canvas
-  //Preloading images and sounds
-  queue = new createjs.LoadQueue(false);
-  queue.installPlugin(createjs.Sound);
-  queue.addEventListener("complete", handleComplete);
-  queue.loadManifest([
-    {id:"part0", src:"https://s3.amazonaws.com/monstertoolbox/eye1.png"},
-    {id:"part1", src:"https://s3.amazonaws.com/monstertoolbox/mouth.png"},
-    {id:"sound", src:"/images/blip.mp3"},
-    ]);
-}
-
 function stop() {
   createjs.Ticker.removeEventListener("tick", tick);
 }
 
-function handleComplete(event) {
-  // Create an array of all elements with class "part-button"
-  var partButton = document.getElementsByClassName("part-button");
-  // Get the length of the array
-  var partTotal = partButton.length;
-  // For each item in the array add an event listener that triggers
-  // "handleclick" function when clicked
-  for(var i = 0; i < partTotal ; i++){
-    partButton[i].addEventListener('click', handleClick);
-  }
-}
+// function handleComplete(event) {
+//   // Create an array of all elements with class "part-button"
+//   var partButton = document.getElementsByClassName("part-button");
+//   // Get the length of the array
+//   var partTotal = partButton.length;
+//   // For each item in the array add an event listener that triggers
+//   // "handleclick" function when clicked
+//   for(var i = 0; i < partTotal ; i++){
+//     partButton[i].addEventListener('click', handleClick);
+//   }
+// }
 function handleClick(event) {
   var image = event.target;
   var bitmap;
+
 
   var container = new createjs.Container();
   stage.addChild(container);
--- app/assets/javascripts/initializer.js
+++ app/assets/javascripts/initializer.js
@@ -4,4 +4,32 @@ monster.initialize = function() {
   $(document).foundation();
 
 
+  canvas = document.getElementById("monsterCanvas");
+  stage = new createjs.Stage(canvas);
+
+  // enable touch interactions if supported on the current device:
+  createjs.Touch.enable(stage);
+
+  // enabled mouse over / out events
+  stage.enableMouseOver(10);
+  stage.mouseMoveOutside = true; // keep tracking the mouse even when it leaves the canvas
+  //Preloading images and sounds
+  queue = new createjs.LoadQueue(false);
+  queue.installPlugin(createjs.Sound);
+  // queue.addEventListener("complete", handleComplete);
+  queue.loadManifest([
+    {id:"part0", src:"https://s3.amazonaws.com/monstertoolbox/eye1.png"},
+    {id:"part1", src:"https://s3.amazonaws.com/monstertoolbox/mouth.png"},
+    {id:"sound", src:"/assets/blip.mp3"},
+    ]);
+
+  //Find all the elements with class "part-button"
+  var partButton = document.getElementsByClassName("part-button");
+  // Get the length of the array
+  var partTotal = partButton.length;
+  // For each item in the array add an event listener that triggers
+  // "handleclick" function when clicked
+  for(var i = 0; i < partTotal ; i++){
+    partButton[i].addEventListener('click', handleClick);
+  }
 };
