0cf4597d9d96ce28aec0b556e39daefdb6ffb378
testkitschen <katryn.mcintosh@gmail.com>
2014-08-26 19:43:32 -0400

    Click on the tab to open a drawer also changes the color of the tab

--- app/assets/javascripts/parts-buttons.js
+++ app/assets/javascripts/parts-buttons.js
@@ -2,72 +2,92 @@
 $( "#eyecategory" ).click(function() {
   if ( $( "#eyes:first" ).is( ":hidden" ) ) {
     $( "#eyes" ).show( "slow" );
+    $( this ).css( "background-color", "rgba(52, 139, 122, 0.69)" );
   } else {
     $( "#eyes" ).slideUp();
+    $( this ).css( "background-color", "black" );
   }
 });
+
+
 // Mouths
 $( "#mouthcategory" ).click(function() {
   if ( $( "#mouths:first" ).is( ":hidden" ) ) {
     $( "#mouths" ).show( "slow" );
+    $( this ).css( "background-color", "rgba(52, 139, 122, 0.69)" );
   } else {
     $( "#mouths" ).slideUp();
+    $( this ).css( "background-color", "black" );
   }
 });
 // Noses
 $( "#nosecategory" ).click(function() {
   if ( $( "#noses:first" ).is( ":hidden" ) ) {
     $( "#noses" ).show( "slow" );
+    $( this ).css( "background-color", "rgba(52, 139, 122, 0.69)" );
   } else {
     $( "#noses" ).slideUp();
+    $( this ).css( "background-color", "black" );
   }
 });
 // Ears
 $( "#earcategory" ).click(function() {
   if ( $( "#ears:first" ).is( ":hidden" ) ) {
     $( "#ears" ).show( "slow" );
+    $( this ).css( "background-color", "rgba(52, 139, 122, 0.69)" );
   } else {
     $( "#ears" ).slideUp();
+    $( this ).css( "background-color", "black" );
   }
 });
 // Horns
 $( "#horncategory" ).click(function() {
   if ( $( "#horns:first" ).is( ":hidden" ) ) {
     $( "#horns" ).show( "slow" );
+    $( this ).css( "background-color", "rgba(52, 139, 122, 0.69)" );
   } else {
     $( "#horns" ).slideUp();
+    $( this ).css( "background-color", "black" );
   }
 });
 // Arms
 $( "#armcategory" ).click(function() {
   if ( $( "#arms:first" ).is( ":hidden" ) ) {
     $( "#arms" ).show( "slow" );
+    $( this ).css( "background-color", "rgba(52, 139, 122, 0.69)" );
   } else {
     $( "#arms" ).slideUp();
+    $( this ).css( "background-color", "black" );
   }
 });
 // Legs
 $( "#legcategory" ).click(function() {
   if ( $( "#legs:first" ).is( ":hidden" ) ) {
     $( "#legs" ).show( "slow" );
+    $( this ).css( "background-color", "rgba(52, 139, 122, 0.69)" );
   } else {
     $( "#legs" ).slideUp();
+    $( this ).css( "background-color", "black" );
   }
 });
 // Tails
 $( "#tailcategory" ).click(function() {
   if ( $( "#tails:first" ).is( ":hidden" ) ) {
     $( "#tails" ).show( "slow" );
+    $( this ).css( "background-color", "rgba(52, 139, 122, 0.69)" );
   } else {
     $( "#tails" ).slideUp();
+    $( this ).css( "background-color", "black" );
   }
 });
 // Hair
 $( "#haircategory" ).click(function() {
   if ( $( "#hair:first" ).is( ":hidden" ) ) {
     $( "#hair" ).show( "slow" );
+    $( this ).css( "background-color", "rgba(52, 139, 122, 0.69)" );
   } else {
     $( "#hair" ).slideUp();
+    $( this ).css( "background-color", "black" );
   }
 });
 
